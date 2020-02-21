4d82ba28ee0c6e8238c902e3eae472ecd41ba9eb
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-02-20 21:30:38 -0500

    Commenting on through process

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,40 +18,46 @@ with open(election_file, newline='') as csvfile:
     # Continue adding votes at the end of the loop (before going to next row)
     
     for row in csvreader:
+        
+        #iterate through unique names and add to dictionary
+        #set up candidate vote count- add one each time candidate is found in reader
         if row[2] in votedictionary:
             candidate_vote= votedictionary.get(row[2],0)[0]+1
+            #dictionary key: candidate name, value (list): [vote count, percent]
             votedictionary[row[2]] = [candidate_vote, (candidate_vote/num_votes)*100]
+        
         else:
+            #if not in dictionary it is the first vote for the candidate (num_votes=1)
             votedictionary[row[2]]=[num_votes,1/num_votes]
+        
+        #before exiting the iteration need to add one to total vote count
         num_votes += 1
 
     
     #determine a winner
-    
-    
-
-    # comparison value
+    #leader starts at 0, will be changed each time candidate vote is greater than last leader value (each iteration leader is subject to change)
     leader = 0
-    # iterate over each unique candidate in dict
+    # iterate through the candidates in dictionary 
     for key, value in votedictionary.items():
-        # if largest vote count so far, store as top vote count
+        # if vote count of candidate is largest so far it is stored as current leader 
         if value[0] > leader:
             leader = value[0]
-            # store corresponding candidate name
+            # store candidate name (if it is current leader)
             winner = key
         else:
             pass
 
 
 #final results- round percentages in f'string
+    print('`')
     print("Election Results")
     print("--------------------------------")
     print(f'Total votes: {(num_votes)}')
     print("--------------------------------")
-    print(f'Khan: %{round((votedictionary["Khan"][1]),5)} ({votedictionary["Khan"][0]})')
-    print(f'Correy: %{round((votedictionary["Correy"][1]),4)} ({votedictionary["Correy"][0]})')
-    print(f'Li: %{round((votedictionary["Li"][1]),4)} ({votedictionary["Li"][0]})')
-    print("O'Tooley: %" + str(round((votedictionary["O'Tooley"][1]),4)) + " (" + str(votedictionary["O'Tooley"][0])+ ")" )
+    print(f'Khan: %{round((votedictionary["Khan"][1]),3)} ({votedictionary["Khan"][0]})')
+    print(f'Correy: %{round((votedictionary["Correy"][1]),3)} ({votedictionary["Correy"][0]})')
+    print(f'Li: %{round((votedictionary["Li"][1]),3)} ({votedictionary["Li"][0]})')
+    print("O'Tooley: %" + str(round((votedictionary["O'Tooley"][1]),3)) + " (" + str(votedictionary["O'Tooley"][0])+ ")" )
     print("--------------------------------")
-    print(f'Winner: {winner}!')
+    print(f'Winner: {winner}')
     print("--------------------------------")
